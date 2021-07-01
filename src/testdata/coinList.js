10ec21cd00c4b2d04d94716b7963947a6bd92500
elvstejd <elvistejeda0910@gmail.com>
2021-07-01 01:10:41 -0400

    Move search list test data to separate file

---
+++ src/testdata/coinList.js
@@ -0,0 +1,132 @@
+const searchList = [
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+    {
+        value: 'DOGE',
+        label: 'DOGE'
+    },
+    {
+        value: 'BTC',
+        label: 'BTC'
+    },
+];
+
+export default searchList;
