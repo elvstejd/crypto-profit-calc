0dafccdca7f5881aa484217705a0c1cc56957d6e
elvstejd <elvistejeda0910@gmail.com>
2021-10-12 00:17:05 -0400

    Cleanup

--- src/components/ProfitDisplay.js
+++ src/components/ProfitDisplay.js
@@ -3,17 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { useData } from '../contexts/dataContext';
 import '../styles/ProfitDisplay.css';
 
-function getAppropiateSize(profitAmount) {
-    if (!profitAmount) return;
 
-    const amount = (profitAmount * 1).toString().length;
-
-    if (amount < 11) return "lg";
-    if (amount <= 13) return "md";
-    if (amount <= 16) return "sm";
-}
-
-const ProfitDisplay = (props) => {
+const ProfitDisplay = () => {
     const { profit } = useData();
     const { t } = useTranslation();
 
@@ -23,7 +14,6 @@ const ProfitDisplay = (props) => {
             <div id="earnings-container">
                 <div
                     className={"earnings"}
-                    // className={"earnings " + getAppropiateSize(props.profit)}
                     id={profit >= 0 ? "profit" : "loss"}
                 >
                     {profit >= 0 ? ("$" + (profit * 1).toLocaleString()) : ("-$" + (profit * -1).toLocaleString())}
