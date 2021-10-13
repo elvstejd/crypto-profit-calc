9a166c552f6ecc320415facd143821f66e352a61
elvstejd <elvistejeda0910@gmail.com>
2021-10-13 16:26:38 -0400

    Remove big.js reference

--- src/utils/parseValue.js
+++ src/utils/parseValue.js
@@ -1,11 +1,5 @@
-import Big from 'big.js';
+
 
 export function parseValue(number) {
-    if (!number) return null;
-    try {
-        const value = new Big(number);
-        return value;
-    } catch (error) {
-        return null;
-    }
+
 }
