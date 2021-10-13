fe652e6f05f4948b3af034d69bb4c1a507b7b266
elvstejd <elvistejeda0910@gmail.com>
2021-10-13 16:24:18 -0400

    Write some tests for key functions

---
+++ src/__tests__/utils/calculateProfit.test.js
@@ -0,0 +1,14 @@
+import { calculateCoinAmount } from "../../utils/calculateCoinAmount";
+import { calculateGrossBalance } from "../../utils/calculateGrossBalance";
+import { calculateProfit } from "../../utils/calculateProfit";
+
+
+// calculateProfit()
+
+test('returns 0 given an amount known to cause trouble is given', () => {
+    const coinAmount = calculateCoinAmount(99999999999, 10.1);
+    const grossBalance = calculateGrossBalance(coinAmount, 10.1)
+    const result = calculateProfit(grossBalance, 99999999999);
+
+    expect(result).toBe("0");
+});
