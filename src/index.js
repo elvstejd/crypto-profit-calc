654dfb0be1d593570c660baca52a7b253d715ffa
elvstejd <elvistejeda0910@gmail.com>
2021-10-14 18:16:01 -0400

    import variables.css file on index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import "./i18nextConf";
+import './styles/variables.css';
+// import './styles/global.css';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App/>, root);
+ReactDOM.render(<App />, root);
