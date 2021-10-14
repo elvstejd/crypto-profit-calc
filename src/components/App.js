61b43e421d91ed7badd5be1bd1fea6f11124276a
elvstejd <elvistejeda0910@gmail.com>
2021-10-14 18:15:24 -0400

    Refactor app component

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,56 @@ import LanguageSelector from './LanguageSelector';
 import ProfitDisplay from './ProfitDisplay';
 import { useTranslation } from 'react-i18next';
 import '../styles/App.css';
-import logo from '../assets/images/logo.png';
+// import logo from '../assets/images/logo.png';
 import DataProvider from '../contexts/dataContext';
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+    max-width: 60rem;
+    min-height: 30rem;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 20rem 40rem;
+    grid-template-rows: 50% 50%;
+    grid-gap: 1rem;
+`;
+
+const TaglineWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    margin-bottom: 0.5rem;
+    margin-top: 2rem;
+
+    p {
+        font-size: 1.2rem;
+        text-align: center;
+    }
+`;
+
+const DisclaimerContainer = styled.div`
+    max-width: 60rem;
+    margin: 0 auto;
+    margin-top: 1.5rem;
+
+    p {
+        color: var(--neutral-200);
+        font-size: small;
+    }
+`;
+
+const StyledFooter = styled.footer`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+`;
+
+const SourceLink = styled.a`
+    color: var(--neutral-200);
+`;
+
 
 const App = () => {
     const { t } = useTranslation();
@@ -14,27 +62,21 @@ const App = () => {
     return (
         <DataProvider>
             <div>
-                <header>
-                    <img src={logo} alt="logo" />
-                </header>
-                <div className="tagline-container">
+                <TaglineWrapper>
                     <p>{t('tagline')}</p>
-                </div>
-                <main>
+                </TaglineWrapper>
+                <StyledMain>
                     <MainForm />
                     <ProfitDisplay />
                     <GrowthForm />
-                </main>
-                <div className="currency-disclaimer-container">
+                </StyledMain>
+                <DisclaimerContainer>
                     <p>*{t("currency_disclaimer")}</p>
-                </div>
-                <footer>
-                    <div className="me">
-                        <p>{t("made_by")}</p>
-                        <p>{t("feedback")}</p>
-                    </div>
+                </DisclaimerContainer>
+                <StyledFooter>
+                    <SourceLink target="_blank" href="https://github.com/elvstejd/crypto-profit-calc">{t("source_code_label")}</SourceLink>
                     <LanguageSelector />
-                </footer>
+                </StyledFooter>
             </div>
         </DataProvider>
     );
