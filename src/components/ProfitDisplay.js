6da8846c302daf4bd376dd4349a9035d60acc404
elvstejd <elvistejeda0910@gmail.com>
2021-10-14 22:21:47 -0400

    Refactor profit display

--- src/components/ProfitDisplay.js
+++ src/components/ProfitDisplay.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 import { useData } from '../contexts/dataContext';
-import '../styles/ProfitDisplay.css';
+import { Card } from '../styles/shared/Card';
 import { formatProfitToUSD } from '../utils/formatProfitToUSD';
 
+const ProfitNumberContainer = styled.div`
+    display: flex;
+    line-height: none;
+    justify-content: center;
+    height: fit-content;
+    margin: auto 0;
+    overflow: hidden;
+`;
+
+const ProfitNumber = styled.div`
+    line-height: none;
+    max-width: inherit;
+    font-size: 5rem;
+    color: ${props => props.isProfit ? 'var(--positive-500)' : 'var(--negative-500)'};
+    overflow-x: auto;
+`;
 
 const ProfitDisplay = () => {
     const { profit } = useData();
     const { t } = useTranslation();
 
     return (
-        <div className="container">
+        <Card>
             <label>{t('profitloss_label')}</label>
-            <div id="earnings-container">
-                <div
-                    className={"earnings"}
-                    id={profit >= 0 ? "profit" : "loss"}
-                >
+            <ProfitNumberContainer>
+                <ProfitNumber isProfit={profit >= 0}>
                     {formatProfitToUSD(profit)}
-                </div>
-            </div>
-        </div>
+                </ProfitNumber>
+            </ProfitNumberContainer>
+        </Card>
     );
 }
 
