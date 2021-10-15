f986ca66efd124bd938f8258d0373523784c49de
elvstejd <elvistejeda0910@gmail.com>
2021-10-15 00:28:45 -0400

    Add borders to inputs

--- src/styles/shared/InputContainer.js
+++ src/styles/shared/InputContainer.js
@@ -7,7 +7,7 @@ export const InputContainer = styled.div`
     background-color: var(--primary-500);
     border-radius: 6px;
     padding: 0.5rem 1rem;
-
+    border: 1px solid var(--primary-200);
     span {
         display: flex;
         align-items: center;
